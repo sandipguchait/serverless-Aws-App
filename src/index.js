9ac245f7c2b47d5b375a91d4c67236ca4994b756
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-10 19:03:51 +0530

    using Amplify.configure() to configure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//imorting aws amplify 
+import Amplify from 'aws-amplify';
+//importing aws configuration files 
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
