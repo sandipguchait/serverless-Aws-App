2c3b7679ed293eabac62169a6de8ca94b5a15ecb
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-10 23:20:11 +0530

    create note mutation on Aws using graphQL

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 //adding authentication for react app
 import { withAuthenticator } from 'aws-amplify-react';
+//using api configuration for graphQl 
+import { API, graphqlOperation } from 'aws-amplify';
+// brining mutation module 
+import { createNote } from './graphql/mutations';
 
 class App extends Component {
 
   state= {
-    notes:[
-      {
-        id: 1,
-        note: "Hello world"
-      }
-    ]
+    termnote:"",
+    notes:[]
+  }
+
+  handleChange = (event) => {
+    this.setState({ termnote: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { termnote } = this.state;
+    const input = { note: termnote }
+    API.graphql(graphqlOperation( createNote, { input }));
   }
 
   render() {
@@ -20,10 +31,12 @@ class App extends Component {
        pa3 bg-washed-red">
        <h1 className="code f2-1">Amplify NoteTaker</h1>
        {/* Note Form */}
-        <form className="mb3">
+        <form className="mb3" onSubmit={this.handleSubmit}>
             <input type="text"
               className="pa2 f4"
-              placeholder="Write Your Note"/>
+              placeholder="Write Your Note"
+              onChange={this.handleChange}
+              />
               <button className="pa2 f4"
               type="submit">
               Add Note 
